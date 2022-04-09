c3e469583fe0d9207033bf178d7d0e4d067e255a
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-04-09 16:47:57 +0200

    setup for writing smart contract finished

---
+++ contracts/first_contract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FirstContract {
+    // add liquidity (deposit any erc20 and receive some LP tokens (erc20 as well))
+
+    // withdraw (burn LP tokens and receive deposited erc20 tokens)
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -7,22 +7,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.5.16"
-      },
-      {
-        version: "0.6.2"
-      },
-      {
-        version: "0.6.4"
-      },
-      {
-        version: "0.7.0"
-      },
-      {
-        version: "0.7.3"
-      },
-      {
-        version: "0.8.0"
+        version: "0.8.2"
       }
     ]
   },
