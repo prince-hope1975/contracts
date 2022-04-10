e5761070bb262e4a38cd5dff4285d15305f342ef
Linus Weigand <linusweigand@protonmail.com>
2022-04-10 12:29:39 +0200

    deploy script

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -2,9 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
-contract DepositAndWithdraw {
+contract Fund {
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,27 @@
-import { 
-    Contract, 
-    ContractFactory 
-  } from "ethers"
-  import { ethers } from "hardhat"
-
-
 const main = async () => {
     // get contract factory from ethers (obtain contract)
     // deploy contract
     // await deployed contract
     // log address where contract deployed to
+    const [deployer] = await hre.ethers.getSigner();
+    
+    console.log("Deploying contracts with account: ", deployer.address);
+
+    const fundContractFactory = await hre.ethers.getContractFactory("Funding");
+    const fundContract = await fundContractFactory.deploy();
+    await fundContract.deployed();
+
+    console.log("Fund contract address: ", fundContract.address);
 }
 
-main()
-    .then(()=>process.exit(0))
-    .catch(error=> {
-        console.error(error)
-        process.exit(1)
-    })
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+};
+
+runMain();
