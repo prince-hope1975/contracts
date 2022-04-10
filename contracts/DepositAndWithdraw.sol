3f3cb2ed56d4b66dc006f984739bab9c7f67b25e
Linus Weigand <linusweigand@protonmail.com>
2022-04-10 12:22:39 +0200

    Deposit and withdraw contract

--- contracts/DepositAndWithdraw.sol
+++ contracts/DepositAndWithdraw.sol
@@ -5,7 +5,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract DepositAndWithdraw {
-    uint value;
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
 
-    constructor() {}
+
+    function fund() public payable {
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function withdraw() public payable {
+       payable(msg.sender).transfer(addressToAmountFunded[msg.sender]);
+    }
+
+    function getFunders() public view returns (address[] memory) {
+        return funders;
+    }
 }
